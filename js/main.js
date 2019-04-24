41b8eb5237d99afa22dc61582c0a4bc3abb12e85
cmflood <42477038+cmflood@users.noreply.github.com>
2019-04-24 17:22:32 -0400

    Adding bubble transition and bar chart

--- js/main.js
+++ js/main.js
@@ -132,6 +132,7 @@
         loadBubble(width, height, firstBubble);
         lineGraph(firstExpressed, width, height);
         createLineDropdown(width, height);
+        groupedBars()
         
         function createBubbleDropdown(width, height){
             //add select element
@@ -158,6 +159,9 @@
         };
     
         function loadBubble(width, height, attribute){
+            var t = d3.transition()
+                .duration(1000);
+            
             d3.select(".bubbleChart")
                 .remove();
             
@@ -230,6 +234,7 @@
 
                 node.append("text")
                     .style("text-anchor", "middle")
+                    .transition(t)
                     .text(function(d) {
                         //if you don't want some of the circles populated with text, specify constraints below
                         if (d.data.value > 748|| d.data.Crime == "Other" || d.data.Crime == "Fire"){
@@ -402,6 +407,216 @@
                 }; //mousemove close           
                 })
         }; // lineGraph close
+        
+    //groupedBars chart
+   function groupedBars(){
+        
+        var data = [
+          {
+            "area": "Antellope Valley",
+            "stats": [234872,58748,15939,75517,7776,7205,1542, 1969,6436,21 ]
+          },
+          {
+            "area": "East",
+            "stats": [856195,59773,179520,422500,27218,27509,10028,5112,17558,67]
+          },
+          {
+            "area": "Metro",
+            "stats": [692630,69543,223782,342703,23433,71453,19855,1041,2800,7]
+          },
+          {
+            "area": "San Fernando",
+            "stats": [1519700,85191,260276,332340,28087,34602,24381,1338,5480,7]
+          },
+          {
+            "area": "San Gabriel Valley",
+            "stats": [858743,69522,536292,372796,21073,29109,20903,3092,10258,25]
+          },
+
+          {
+            "area": "South",
+            "stats": [450301,319302,36972,450731,32145,24587,8414,2958,9398,92]
+          },
+        {
+            "area": "South Bay",
+            "stats": [819567,276700,240154,276432,25022,27350,14769, 2805,9004,49]
+          },
+
+          {
+            "area": "West",
+            "stats": [570986,63157,99725,41237,4622,10221,19233,239,2089,4]
+          }
+        
+        ];
+
+        var ids = ['white', 'black', 'asian', 'other', 'lessHS', 'HSgrad', 'BAhigher', 'narcotics', 'BurgRob','homicide'];
+        var demVar = ['White', 'Black', 'Asian', 'Other', 'Less than High School Degree', 'High School Degree', 'BA or Higher', 'Narcotics', 'Burglary/Robbery','Homicides'];
+
+        // Let's populate the categoeries checkboxes
+        d3.select('.categories').selectAll('.checkbox')
+          .data(ids)
+          .enter()
+          .append('div')
+          .attr('class', 'checkbox')
+          .append('label').html(function(id, index) {
+            var checkbox = '<input id="' + id + '" type="checkbox" class="category">';
+            return checkbox + demVar[index];
+          });
+
+        // some variables declarations
+        var margin = {top: 20, right: 20, bottom: 30, left: 95},
+            width = 800 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
+        
+        // the scale for the area value
+        var x = d3.scaleLinear()
+                .rangeRound([0, width]);
+
+        // the scale for each state
+        var y0 = d3.scaleBand()
+                .rangeRound([0, height], .2);
+                //.bandwidth([0, height], .1);
+        // the scale for each state age
+        var y1 = d3.scaleBand();
+
+        // just a simple scale of colors
+        var color = d3.scaleOrdinal()
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00", "#ca0020","#f4a582","#d5d5d5"]);
+
+        //
+        var xAxis =  d3.axisBottom()
+            .scale(x)
+            .tickFormat(d3.format(".2s"));
+
+        var yAxis = d3.axisLeft()
+            .scale(y0);
+
+        var svg = d3.select(".graph").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        d3.select('.categories').selectAll('.category').on('change', function() {
+           var x = d3.select('.categories').selectAll('.category:checked');
+            //console.log("x>>>>>>>>>>>>>>", x._groups)
+            var ids = [];
+            for(var i=0; i<x._groups[0].length; i++) {
+                ids = [...ids, x._groups[0][i].id]
+                console.log(x._groups[0][i])
+            }
+                console.log('>>>>>>>>>>>>>>>>>>>>>>', ids)
+          updateGraph(ids);
+            });
+        
+        renderGraph();
+
+        function renderGraph() {
+          x.domain([0, 0]);
+          // y0 domain is all the state names
+          y0.domain(data.map(function(d) { return d.area; }));
+          // y1 domain is all the age names, we limit the range to from 0 to a y0 band
+          y1.domain(demVar).rangeRound([0, y0.bandwidth()]);
+
+          svg.append("g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+          svg.append("g")
+              .attr("class", "y axis")
+              .call(yAxis)
+        }
+
+        function updateGraph(selectedIds) {
+
+          var areasData = data.map(function(areaData) {
+            return {
+              area: areaData.area,
+              attributes: selectedIds.map(function(selectedId) {
+                var index = ids.findIndex(function(id) {
+                  return selectedId === id;
+                });
+                return {
+                  id: ids[index],
+                  name: demVar[index],
+                  value: areaData.stats[index]
+                };
+              })
+            }
+    
+          });
+
+
+          // x domain is between 0 and the maximun value in any ages.value
+          x.domain([0, d3.max(areasData, function(d) { return d3.max(d.attributes, function(d) { return d.value }); })]);
+          // y0 domain is all the state names
+          y0.domain(areasData.map(function(d) { return d.area; }));
+          // y1 domain is all the age names, we limit the range to from 0 to a y0 band
+          y1.domain(ids).rangeRound([0, y0.bandwidth()]);
+
+          svg.selectAll('.axis.x').call(xAxis);
+          svg.selectAll('.axis.y').call(yAxis);
+
+          var area = svg.selectAll(".area")
+            .data(areasData);
+
+          area.enter().append("g")
+            .attr("class", "area")
+            .attr("transform", function(d) { return "translate(0, " + y0(d.area) + ")"; });
+
+          var attribute = area.selectAll("rect")
+            .data(function(d) { return d.attributes; });
+
+          // we append a new rect every time we have an extra data vs dom element
+          attribute.enter().append("rect")
+            .attr('width', 0);
+
+          // this updates will happend neither inserting new elements or updating them
+          attribute
+            .attr("x", 0)
+            .attr("y", function(d, index) { return y1(ids[index]); })
+            .attr("id", function(d) { return d.id; })
+            .style("fill", function(d) { return color(d.name); })
+            .text(function(d) { return d.name })
+            .transition()
+            .attr("width", function(d) { return x(d.value); })
+            .attr("height", y1.bandwidth());
+
+          attribute.exit().transition().attr("width", 0).remove();
+
+          var legend = svg.selectAll(".legend")
+              .data(areasData[0].attributes.map(function(attribute) { return attribute.name; }));
+
+          legend.enter().append("g");
+          legend
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(0," + (200 + i * 20) + ")"; });
+
+          var legendColor = legend.selectAll('.legend-color').data(function(d) { return [d]; });
+          legendColor.enter().append("rect");
+          legendColor
+            .attr('class', 'legend-color')
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+          var legendText = legend.selectAll('.legend-text').data(function(d) { return [d]; });;
+
+          legendText.enter().append("text");
+          legendText
+            .attr('class', 'legend-text')
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d; });
+
+          legend.exit().remove();
+        }
+        
+    } //groupedBars close
         
         
 }); //all wrapper function close
